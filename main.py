11a168b82941ba482890ea61be61e94310f80733
Olanrewaju Oladimeji <140918800+Horladimzzy001@users.noreply.github.com>
2023-08-06 22:53:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-from turtle import Turtle, Screen
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
 
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
-tim = Turtle()
+user_1 = User("001", "angela")
+user_2 = User("002", "jack")
 
-decf = 0
-screen = Screen()
-
-def move_forward():
-    tim.forward(20)
-
-screen.listen()
-
-screen.onkey(key="space", fun=move_forward)
-
-screen.exitonclick()
-
-#still not done with this code
-for items in range(13):
-    decf += 1
-    print(items)
-
-print(decf)
+user_1.follow(user_2)
+print(user_2.followers)
